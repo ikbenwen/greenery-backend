76418b21723cccb12a80bf13b628c41553d2cfab
ikbenwen <w.michels@novi-education.nl>
2021-02-19 17:53:25 +0100

    first commit

---
+++ src/main/java/nl/wendymichels/springboot/greenery/controller/AuthController.java
@@ -0,0 +1,49 @@
+package nl.wendymichels.springboot.greenery.controller;
+
+import nl.wendymichels.springboot.greenery.payload.request.LoginRequest;
+//import nl.wendymichels.springboot.greenery.payload.request.SetAvatarRequest;
+import nl.wendymichels.springboot.greenery.payload.request.SignupRequest;
+import nl.wendymichels.springboot.greenery.payload.response.JwtResponse;
+import nl.wendymichels.springboot.greenery.payload.response.MessageResponse;
+import nl.wendymichels.springboot.greenery.service.AuthorizationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Uitleg over CrossOrigin en CORS:
+ * https://medium.com/@baphemot/understanding-cors-18ad6b478e2b
+ *
+ * Gebruik in Spring-boot (op controller en globally)
+ * https://www.tutorialspoint.com/spring_boot/spring_boot_cors_support.htm
+ *
+ * Zoals je hieronder ziet, kun je ook op klasse-niveau een adres configureren. Iaw alle methodes hieronder, hebben
+ * /api/auth voor de link staan.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthorizationService authorizationService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtResponse> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        return authorizationService.authenticateUser(loginRequest);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<MessageResponse> registerUser(@RequestBody SignupRequest signUpRequest) {
+        return authorizationService.registerUser(signUpRequest);
+    }
+
+    //
+//    @PutMapping ("/profile/{id}")
+//    public ResponseEntity <MessageResponse> updateUser(@RequestBody SetAvatarRequest setAvatarRequest) {
+//        return authorizationService.updateUser(setAvatarRequest);
+//    }
+
+
+}
---
+++ src/test/java/nl/wendymichels/springboot/greenery/NoviExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.wendymichels.springboot.greenery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoviExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
