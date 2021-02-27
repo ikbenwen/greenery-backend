4201fef3be096e531ea3c2ccb06e63a26b2ebc12
ikbenwen <w.michels@novi-education.nl>
2021-02-27 14:40:02 +0100

    profile pictures

--- src/main/java/nl/wendymichels/springboot/greenery/DatabaseFiller.java
+++ src/main/java/nl/wendymichels/springboot/greenery/DatabaseFiller.java
@@ -29,7 +29,7 @@ public class DatabaseFiller implements CommandLineRunner {
         admin.setUsername("admin");
         admin.setEmail("admin@admin.nl");
         admin.setPassword("123456");
-        admin.setAvatar("https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg");
+        admin.setAvatar("https://image.jimcdn.com/app/cms/image/transf/dimension=194x1024:format=png/path/sd273fcf2e85778d5/image/i2af7c972b9882e61/version/1605102367/image.png");
         admin.setRole(rollen);
         authorizationService.registerUser(admin);
 
@@ -37,7 +37,7 @@ public class DatabaseFiller implements CommandLineRunner {
         mod.setUsername("mod");
         mod.setEmail("mod@mod.nl");
         mod.setPassword("123456");
-        mod.setAvatar("https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg");
+        mod.setAvatar("https://activiteiten.rozet.nl/storage/media/activities/165/4iN5dKbCYI18esLvPkO7UWuqULlp0gklRGIjOOlY.jpg");
         rollen.remove("admin");
         rollen.add("mod");
         mod.setRole(rollen);
@@ -48,7 +48,7 @@ public class DatabaseFiller implements CommandLineRunner {
         user.setUsername("user");
         user.setEmail("user@user.nl");
         user.setPassword("123456");
-        user.setAvatar("https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg");
+        user.setAvatar("https://i.pinimg.com/736x/7b/89/e2/7b89e2cef3dff24825185e74d06db34b.jpg");
         rollen.remove("mod");
         rollen.add("user");
         user.setRole(rollen);
@@ -58,7 +58,7 @@ public class DatabaseFiller implements CommandLineRunner {
         superuser.setUsername("superuser");
         superuser.setEmail("super@user.nl");
         superuser.setPassword("123456");
-        superuser.setAvatar("https://www.imgworlds.com/wp-content/uploads/2015/12/18-CONTACTUS-HEADER.jpg");
+        superuser.setAvatar("https://pbs.twimg.com/media/BkjEhsaIcAAo3_1.jpg");
         rollen.add("mod");
         rollen.add("admin");
         superuser.setRole(rollen);
